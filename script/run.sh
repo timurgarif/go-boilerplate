0ff80d66b461ab52af13702b3a99deabeafee8d0
Timur Garifulin <tgarifulin@gmail.com>
2020-09-01 11:57:34 +0300

    Remove unrelated content

--- script/run.sh
+++ script/run.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-export CGO_LDFLAGS='-s -w -L/usr/local/lib -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy -llz4 -lzstd' 
-export ORIGIN_HOST=https://test.liteka.com
-
 golangci-lint run ./...
 echo 'lint OK'
 go vet ./...
